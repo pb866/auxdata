1f899f8e5c39d970b1581a1af43064c331ffe947
Peter Bräuer <pb866@york.ac.uk>
2017-12-16 00:53:15 +0000

    Updated plot functions in jl.mod/make_plots
    Revised module make_plots:
    Revise colour scheme selection function sel_ls, now allows the selection
    of different colour schemes by keyword (currently "line", "sink", and
    "source"). Additional colors and line styles with 20 group members
    including dash-dot-dotted lines.
    Removal of function compile_pdf, which is replaced by the pdf backend
    from matplotlib.
    Adjustments in lineplot to the use of pdf instead of compile_pdf, now
    the plot data is return instead of saving the plot in a pdf. No function
    parameter for file name anymore. Adjustments in colour and line style
    settings with more colours and line styles from sel_ls. Adjusted minimum
    values for y values (now only set to 0, if negative).
    New routines plot_prodloss and plot_flux to plot time-resolved sink and
    source fluxes (or either sink or source fluxes).

--- jl.mod/make_plots.jl
+++ jl.mod/make_plots.jl
@@ -2,24 +2,31 @@ __precompile__()
 
 
 """
-  # Module make_plots
+# Module make_plots
 
-  Generate plots from DSMACC model output
+Generate plots from DSMACC model output.
+
+# Functions (public)
+- lineplot
+- plot_flux
+- plot_prodloss
+- sel_ls
 """
 module make_plots
 
 
-  ##################
-  ###  PREAMBLE  ###
-  ##################
+##################
+###  PREAMBLE  ###
+##################
 
-  # Import external modules
-  using PyPlot, DataFrames
+# Import external modules
+using PyPlot, DataFrames
 
-  # Export public functions
-  export lineplot,
-         compile_pdf,
-         sel_ls
+# Export public functions
+export lineplot,
+       plot_flux,
+       plot_prodloss,
+       sel_ls
 
 
 ###################
@@ -28,29 +35,27 @@ module make_plots
 
 
 """
-    lineplot(xdata,ydata,label,what,unit,icase,plotdata,fname::String="DSMACC.pdf")
+    lineplot(xdata,ydata,label,what,unit,icase,plotdata)
 
-  Generate a line plots from `xdata` and `ydata` with specified `label`s
-  and in specified `unit`s into the output file `fname`.
+Generate line plot data from `xdata` and `ydata` with specified `label`s
+and in specified `unit`s and return the raw plot data.
 
-  `what` specifies, whether to plot `"specs"` or `"rates"`, `icase` specifies the
-  current scenario(s), `plotdata` the species/reactions to be plotted.
+`what` specifies, whether to plot `"specs"` or `"rates"`, `icase` specifies the
+current scenario(s), `plotdata` the species/reactions to be plotted.
+
+The function returns a PyCall.PyObject with the plot data.
 """
-function lineplot(xdata,ydata,label,what,unit,
-                  icase,plotdata,fname::String="DSMACC.pdf")#; unit::String="cm-3"
+function lineplot(xdata,ydata,label,what,unit,icase,plotdata)
 
   # Initialise current plot
   fig, ax = subplots()
   maxval = [] #maximum value needed for axis formats
   ip = 0 #counter for y data needed to assign line style
-  # Loop over different species/rates
-  for n = 1:length(plotdata)
-    # Loop over scenarios
-    for m in icase
-      # Retrieve maximum for current y dataset
-      push!(maxval,maximum(ydata[m][Symbol(plotdata[n])]))
-    end
-  end
+  # Loop over different species/rates in all scenarios
+  for n = 1:length(plotdata)  for m in icase
+    # Retrieve maximum for current y dataset
+    push!(maxval,maximum(ydata[m][Symbol(plotdata[n])]))
+  end  end
   # Retrieve order of overall maximum
   p=floor(log10(maximum(maxval)))
   factor = 10^-p
@@ -72,20 +77,21 @@ function lineplot(xdata,ydata,label,what,unit,
     end
     # Generate plots
     ip += 1
-    clr, lin = sel_ls(ip,ip) #select line styles
+    clr, dt = sel_ls(cs="line",nc=ip,nt=ip) #select line styles
     # plot data
     plot(xdata,ydata[case][Symbol(spc)].*factor,label="$spc ($(label[case]))",
-         color=clr, linestyle=lin)
+         color=clr, dashes=dt)
   end  end
 
   # Format plots
   # Axes ticks, min/max
   ax[:axes][:get_xaxis]()[:set_ticks](collect(0:12:xdata[end]))
-  ax[:set_xlim](xmin=xdata[1], xmax=xdata[end])
+  xlim(xmin=xdata[1], xmax=xdata[end])
   minorticks_on()
   mx = matplotlib[:ticker][:MultipleLocator](3) # Define interval of minor ticks
   ax[:xaxis][:set_minor_locator](mx)
-  ax[:set_ylim](ymin=0)
+  ymin, ymax = ylim()
+  if ymin < 0  ylim(ymin=0)  end
   # Axes labels
   xlabel("model time / hours")
   if unit=="mlc"
@@ -94,6 +100,7 @@ function lineplot(xdata,ydata,label,what,unit,
     elseif what=="rates"  yl = "rate /cm\$^{3\\cdot n}\$ molecules\$^{-n}\$ s\$^{-1}\$"
     end
   else
+    # Labels for converted units with volume mixing ratios
     if what=="specs" yl = "volume mixing ratio / $unit"
     elseif what=="rates"  yl = "rate / $unit\$^{-n+1}\$ h\$^{-1}\$"
     end
@@ -103,46 +110,147 @@ function lineplot(xdata,ydata,label,what,unit,
   legend() #ncol=2
   grid(linestyle=":")
   tight_layout()
-  # Save plot to pdf
-  savefig(fname)
+  # Save and return plot data
+  close(fig)
+  return fig
 end #function lineplot
 
 
 """
-    compile_pdf(iofolder)
+    plot_flux(spc, scenario, modtime, fluxes, cs)
 
-Compile single png plots in one pdf file
-and warn about possible missing software on failure.
+Plot `fluxes` (positive or negative) over model time (`modtime`) using the colour
+scheme `cs` as time-resolved stacked area plot labelling the current species `spc`
+in the y axis and the current `scenario` in the title.
+
+The function returns a PyCall.PyObject with the plot data.
 """
-function compile_pdf(iofolder::String=".",fname::String="plots",ext::String="pdf";
-                     ndel::Int64=0)
+function plot_flux(spc, scenario, modtime, fluxes, cs)
+  # Generate stacked subplots for source and sink fluxes
+  fig, ax = subplots()
+  # Define dotted grid lines
+  ax[:grid](linestyle=":")
 
-  try
-    # compile single plots in a one overall pdf and delete all single plots
-    run(`convert -density 300 $iofolder/'*'.$ext -quality 100 $fname.pdf`)
-    for i = 1:ndel  rm("$iofolder/$(lpad(i,4,"0")).$ext",force=true)  end
-  catch
-    # if compilation fails issue warning about possible missing software
-    println("\033[95mWARNING! UNIX tool 'convert' missing or unable to process plots.")
-    println("No concatenation of plots in single pdf in results folder.")
-    println("\033[96mMac users install imagemagick, e.g. using homebrew:")
-    println("brew install imagemagick\033[0m")
-  end
-end #function compile_pdf
+  # Find maximum
+  p=floor(log10(maximum(abs.(sum(fluxes[1],1)))))
+  # Redesign data
+  fluxes[1] .*= 10^-p
+
+
+  # Fill plots with graphs
+  ax[:stackplot](modtime,fluxes[1],colors=cs)
+  # Define title and legend
+  title("Scenario $scenario")
+  ax[:legend](fluxes[2],fontsize=8)
+  # Axes ticks, min/max, adjust maximum for rounding errors with +.00001
+  ax[:axes][:get_xaxis]()[:set_ticks](collect(0:12:modtime[end]+.00001))
+  xlim(xmin=modtime[1], xmax=modtime[end]+.00001)
+  minorticks_on()
+  mx = matplotlib[:ticker][:MultipleLocator](3) # Define interval of minor ticks
+  ax[:xaxis][:set_minor_locator](mx)
+  # Axes labels
+  xlabel("model time / hours")
+  ax[:set_ylabel]("$spc fluxes / \$10^{$(Int(p))}\$ molecules cm\$^{-3}\$ s\$^{-1}\$")
+  # Final layout settings
+  tight_layout()
+
+  close(fig)
+  # Reset data
+  fluxes[1] ./= 10^-p
+  # Return plot raw data for further pyplot processing
+  return fig
+end #function plot_flux
 
 
 """
-    sel_ls(nc::Int64=1,nt::Int64=1)
+    plot_flux(spc, scenario, modtime, source, sink)
 
-  Select line color and type from a preset list with up to 8 different line styles.
+Plot `source` (positive) and `sink` (negative) data over model time (`modtime`)
+as time-resolved stacked area plot labelling the current species `spc`
+in the y axis and the current `scenario` in the title.
+
+The function returns a PyCall.PyObject with the plot data.
 """
-function sel_ls(nc::Int64=1,nt::Int64=1)
-  # Manually define line colours and styles
-  lc = ["red","blue","green","#FF8533","#FF00FF","#9400D3","black","#A0A0A0"]
-  lt = ["-","--","-.",":","-","--","-.",":"]
+function plot_prodloss(spc, scenario, modtime, source, sink)
+  # Generate stacked subplots for source and sink fluxes
+  fig, (ax1, ax2) = subplots(2, sharex=true)
+  # Define grid in both subplots
+  ax1[:grid](linestyle=":"); ax2[:grid](linestyle=":")
+
+  # Find maximum
+  p=floor(log10(max(maximum(sum(source[1],1)),minimum(sum(sink[1],1)))))
+  # Redesign data
+  source[1] .*= 10^-p; sink[1] .*= -10^-p
+
+
+  # Set colour schemes
+  cs_src, dt = sel_ls(cs="source",nc=1:length(source[2]))
+  cs_snk, dt = sel_ls(cs="sink",nc=1:length(sink[2]))
+  # Fill plots with graphs
+  ax1[:stackplot](modtime,source[1],colors=cs_src)
+  ax2[:stackplot](modtime,sink[1],colors=cs_snk)
+
+  # Define title and legend
+  ax1[:set_title]("Scenario $scenario")
+  ax1[:legend](source[2],fontsize=8); ax2[:legend](sink[2],fontsize=8)
+  # Axes ticks, min/max,\n adjust maximum for rounding errors with +.00001
+  ax2[:axes][:get_xaxis]()[:set_ticks](collect(0:12:modtime[end]+.00001))
+  xlim(xmin=modtime[1], xmax=modtime[end]+.00001)
+  minorticks_on()
+  mx = matplotlib[:ticker][:MultipleLocator](3) # Define interval of minor ticks
+  ax2[:xaxis][:set_minor_locator](mx)
+  # Axes labels
+  xlabel("model time / hours")
+  ax2[:set_ylabel]("$spc sink and source fluxes /\n\$10^{$(Int(p))}\$ molecules cm\$^{-3}\$ s\$^{-1}\$",ha="left")
+  # Final layout settings
+  tight_layout()
+  subplots_adjust(hspace=0)
+
+  close(fig)
+  # Reset data
+  source[1] ./= 10^-p; sink[1] ./= -10^-p
+  # Return plot raw data for further pyplot processing
+  return fig
+end #function plot_flux
+
+
+"""
+    sel_ls(;cs::String="line",nc=1,nt=1)
+
+Select colours with index `nc` from a colour scheme with the key word `cs`
+(currently `"line"`, `"sink"`, and `"source"` available) and a line type
+(from a range of solid, dashed, dash-dotted, and dotted lines) with the index `nt`.
+
+Single colours/line types using integers or subsets using unit ranges `m:n` may be used.
+"""
+function sel_ls(;cs::String="line",nc=1,nt=1)
+  # Manually define sets of colour schemes
+  lin = ["black", "red", "blue", "green", "#FF8533",
+         "#D499ED", "#AC6012", "#FF00FF", "#A50029", "#808000",
+         "#A0A0A0", "#FF7F7F", "#3D9EFF", "#99CC99", "#FBC96D",
+         "#7F7FFF", "#DDBFA0", "#FFB2FF", "#C68696", "#D8D8B2"]
+  src = ["#9900CC", "#003DF5", "#7547FF", "#33FFCC", "#66FF33", "#009933", "#998000",
+         "#990066", "#330099", "#006699", "#00FFFF", "#00FF80", "#99CC66", "#6699CC",
+         "#9966CC", "#CC66CC", "#B83DB8", "#8A2E8A", "#0033FF", "#000088"]
+  snk = ["#FF0000", "#FF8000", "#FFFF00", "#FF7A7A", "#FF0080", "#FF00FF", "#CC0099",
+         "#CC0033", "#CC3300", "#FF470A", "#FFA347", "#FFCC99", "#FF9999", "#FF99CC",
+         "#FFFF99", "#EEB702", "#AA8C2C", "#6D5A1C", "#B7AD8E", "#E69138"]
+  # Combine colour schemes in a dataframe
+  colourschemes = DataFrame(line=lin, source=src, sink=snk)
+  # Save selected colour or subset of colours
+  lc = colourschemes[Symbol(cs)][nc]
+  # Manually define line types
+  dt = [[],[3,2,3,2],[4,1.6,1,1.6,1,1.6],[6,2,1,1,1,1,1,2],[1,1.6,1,1.6],
+        [3,1.6,3,1.6,1,1.6],[4,1.6,1,1.6,1,1.6,4,1.6,1,1.6,1,1.6,1,1.6],
+        [4,1.6,4,1.6,1,1.6,1,1.6,4,1.6,4,1.6,1,1.6],
+        [4,1.6,4,1.6,4,1.6,1,1.6,1,1.6],[5,1.6,2,1.6],
+        [],[3,2,3,2],[4,1.6,1,1.6,1,1.6],[6,2,1,1,1,1,1,2],[1,1.6,1,1.6],
+        [3,1.6,3,1.6,1,1.6],[4,1.6,1,1.6,1,1.6,4,1.6,1,1.6,1,1.6,1,1.6],
+        [4,1.6,4,1.6,1,1.6,1,1.6,4,1.6,4,1.6,1,1.6],
+        [4,1.6,4,1.6,4,1.6,1,1.6,1,1.6],[5,1.6,2,1.6]]
 
   # Return a set of colours/styles depending on the input choice
-  return lc[nc], lt[nt]
+  return lc, dt[nt]
 end #function sel_ls
 
-end #module JLplot
+end #module make_plots
