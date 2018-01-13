cc8f6339f7618c17f2165b8ebf17f0ce7c90b0f2
Peter Bräuer <pb866@york.ac.uk>
2018-01-13 14:04:47 +0000

    Stacked area plots in make_plots.jl
    New function to the concentrations of lumped species as stacked area
    plots with boundary lines. Different colour schemes and line types are
    used for up to 2 different scenarios.
    Allowing an additional keyword "cm-3" to the existing one "mlc" for the
    standard unit molecules per centimetre cubed.

--- jl.mod/make_plots.jl
+++ jl.mod/make_plots.jl
@@ -24,6 +24,7 @@ using PyPlot, DataFrames
 
 # Export public functions
 export lineplot,
+       plot_stack,
        plot_flux,
        plot_prodloss,
        sel_ls
@@ -63,7 +64,7 @@ function lineplot(xdata,ydata,label,what,unit,icase,plotdata)
   # Loop over species/reactions and scenarios
   for spc in plotdata  for case in icase
     # Unit conversions
-    if unit!="mlc"
+    if unit!="mlc"  ||  unit!="cm-3"
       p = 0
       factor = 1./ydata[case][:M]
       if what == "rates"  factor *= 3600.  end
@@ -94,7 +95,7 @@ function lineplot(xdata,ydata,label,what,unit,icase,plotdata)
   if ymin < 0  ylim(ymin=0)  end
   # Axes labels
   xlabel("model time / hours")
-  if unit=="mlc"
+  if unit=="mlc"  ||  unit=="cm-3"
     # Default y label
     if what=="specs" yl = "concentration / 10\$^{$(Int(p))}\$ molecules cm\$^{-3}\$"
     elseif what=="rates"  yl = "rate /cm\$^{3\\cdot n}\$ molecules\$^{-n}\$ s\$^{-1}\$"
@@ -117,6 +118,74 @@ end #function lineplot
 
 
 """
+    plot_stack(xdata,ylines,ystack,scenario,label,unit,lt)
+
+Plot the concentrations (mainly of lumped sum species) as a stacked area plots
+with boundaries using `xdata` and `ylines` for the boundaries and `ystack` for
+the area in the graphs.
+
+The colour schemes and boundary line types are defined in tuple `lt` with the
+colour scheme as String array as first entry and the line types with specifications
+for `dashes` as second entry.
+
+The `scenario` names are printed in the titel. Furthermore, `label`s are us for
+the legend and `unit` is used for the y axis label.
+
+The function returns a PyCall.PyObject with the plot data.
+"""
+function plot_stack(xdata,ylines,ystack,scenario,label,unit,lt)
+
+  # Initialise current plot
+  fig, ax = subplots()
+
+  # Find y-axis maximum
+  if unit!="mlc"  ||  unit!="cm-3"
+    p = 0
+  else
+    p=floor(log10(maximum(ylines[end])))
+    if p==Inf || p==-Inf  p = 0  end
+  end
+  # Redesign data
+  ystack .*= 10^-p; ylines .*= 10^-p
+
+  # Fill plots with graphs
+  ax[:stackplot](xdata,ystack,colors=lt[1],alpha=0.3)
+  ax[:legend](label,fontsize=8) #ncol=2
+  for l = 1:length(ylines)
+    ax[:plot](xdata,ylines[l],color=lt[1][l],dashes=lt[2])
+  end
+
+  ### Format plots ###
+  # Axes ticks, min/max
+  ax[:axes][:get_xaxis]()[:set_ticks](collect(0:12:xdata[end]))
+  xlim(xmin=xdata[1], xmax=xdata[end])
+  minorticks_on()
+  mx = matplotlib[:ticker][:MultipleLocator](3) # Define interval of minor ticks
+  ax[:xaxis][:set_minor_locator](mx)
+  ymin, ymax = ylim()
+  if ymin < 0  ylim(ymin=0)  end
+  # Axes labels
+  xlabel("model time / hours")
+  if unit=="mlc"  ||  unit=="cm-3"
+    # Default y label
+    yl = "concentration / 10\$^{$(Int(p))}\$ molecules cm\$^{-3}\$"
+  else
+    # Labels for converted units with volume mixing ratios
+    yl = "volume mixing ratio / $unit"
+  end
+  ylabel(yl)
+  # Legend, grid, general layout
+  title("Scenario $scenario")
+  ax[:grid](linestyle=":")
+  tight_layout()
+  # Save and return plot data
+  close(fig)
+  return fig
+
+end #function plot_stack
+
+
+"""
     plot_flux(spc, scenario, modtime, fluxes, cs)
 
 Plot `fluxes` (positive or negative) over model time (`modtime`) using the colour
@@ -132,7 +201,8 @@ function plot_flux(spc, scenario, modtime, fluxes, cs)
   ax[:grid](linestyle=":")
 
   # Find maximum
-  p=floor(log10(maximum(abs.(sum(fluxes[1],1)))))
+  p=floor(log10(maximum(sum(fluxes[1],1))))
+  if p==Inf || p==-Inf  p = 0  end
   # Redesign data
   fluxes[1] .*= 10^-p
 
@@ -151,6 +221,11 @@ function plot_flux(spc, scenario, modtime, fluxes, cs)
   # Axes labels
   xlabel("model time / hours")
   ax[:set_ylabel]("$spc fluxes / \$10^{$(Int(p))}\$ molecules cm\$^{-3}\$ s\$^{-1}\$")
+  # annotate missing fluxes in the plot
+  if fluxes[3] != "no fluxes"
+    annotate("omitted fluxes:\n$(join(fluxes[3],'\n'))",
+      xy=[2,0.95⋅ax[:get_ylim]()[2]], verticalalignment="top",fontsize=8)
+  end
   # Final layout settings
   tight_layout()
 
@@ -163,7 +238,7 @@ end #function plot_flux
 
 
 """
-    plot_flux(spc, scenario, modtime, source, sink)
+    plot_prodloss(spc, scenario, modtime, source, sink)
 
 Plot `source` (positive) and `sink` (negative) data over model time (`modtime`)
 as time-resolved stacked area plot labelling the current species `spc`
@@ -178,7 +253,7 @@ function plot_prodloss(spc, scenario, modtime, source, sink)
   ax1[:grid](linestyle=":"); ax2[:grid](linestyle=":")
 
   # Find maximum
-  p=floor(log10(max(maximum(sum(source[1],1)),minimum(sum(sink[1],1)))))
+  p=floor(log10(min(maximum(sum(source[1],1)),maximum(sum(sink[1],1)))))
   # Redesign data
   source[1] .*= 10^-p; sink[1] .*= -10^-p
 
@@ -196,12 +271,28 @@ function plot_prodloss(spc, scenario, modtime, source, sink)
   # Axes ticks, min/max,\n adjust maximum for rounding errors with +.00001
   ax2[:axes][:get_xaxis]()[:set_ticks](collect(0:12:modtime[end]+.00001))
   xlim(xmin=modtime[1], xmax=modtime[end]+.00001)
+  pextr=maximum(sum(source[1][:,2:end],1))
+  nextr=abs(minimum(sum(sink[1][:,2:end],1)))
+  pp=10^floor(log10(pextr))
+  pn=10^floor(log10(nextr))
+  ymin = -ceil(nextr/pn)⋅pn
+  ymax = ceil(pextr/pp)⋅pp
   minorticks_on()
   mx = matplotlib[:ticker][:MultipleLocator](3) # Define interval of minor ticks
   ax2[:xaxis][:set_minor_locator](mx)
+  ax1[:set_ylim](0,ymax); ax2[:set_ylim](ymin,0)
   # Axes labels
   xlabel("model time / hours")
   ax2[:set_ylabel]("$spc sink and source fluxes /\n\$10^{$(Int(p))}\$ molecules cm\$^{-3}\$ s\$^{-1}\$",ha="left")
+  # annotate missing fluxes in the plot
+  if source[3] != "no fluxes"
+    ax1[:annotate]("omitted sources:\n$(join(source[3],'\n'))",
+      xy=[2,0.95⋅ax1[:get_ylim]()[2]], verticalalignment="top",fontsize=8)
+  end
+  if sink[3] != "no fluxes"
+    ax2[:annotate]("ommitted sinks:\n$(join(sink[3],'\n'))",
+      xy=[2,0.95⋅ax2[:get_ylim]()[1]],fontsize=8)
+  end
   # Final layout settings
   tight_layout()
   subplots_adjust(hspace=0)
